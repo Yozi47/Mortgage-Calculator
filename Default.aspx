18ecb075a0a79d9f76a17f61ec91aff957113f08
Yozi47 <ybradhik86@gmail.com>
2021-02-04 12:33:16 -0600

    First commit half way there

--- Default.aspx
+++ Default.aspx
@@ -1,42 +1,98 @@
 ﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Mortgage_Calculator._Default" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <div style="height: 703px; width: 810px; margin-left: 15%; margin-top: 20px; margin-right: 15%;">
+      
+        <div style="height: 50px">
+        <div style="height: 21px; width: 160px; margin-left: 9px; margin-top: 19px;">
+            <asp:Label ID="Label1" runat="server" Text="Mortgage Amount"></asp:Label>
+        </div>
+        <div style="height: 0px; margin-top: -22px; width: 223px; margin-left: 200px;">          
+            <asp:TextBox ID="TextBox1" runat="server" Height="20px" Width="176px" TextMode="Number" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+        </div>
+        </div>
 
-    <div class="jumbotron">
-        <h1>ASP.NET</h1>
-        <p class="lead">ASP.NET is a free web framework for building great Web sites and Web applications using HTML, CSS, and JavaScript.</p>
-        <p><a href="http://www.asp.net" class="btn btn-primary btn-lg">Learn more &raquo;</a></p>
+
+        <div style="height: 50px">
+        <div style="height: 21px; width: 160px; margin-left: 9px; margin-top: 19px;">
+             <asp:Label ID="Label2" runat="server" Text="Length of Years"></asp:Label>
+        </div>
+        <div style="height: 0px; margin-top: -22px; width: 223px; margin-left: 200px;">          
+            <asp:TextBox ID="TextBox2" runat="server" Height="20px" Width="176px" TextMode="Number" OnTextChanged="TextBox2_TextChanged" ></asp:TextBox>
+        </div>
+        </div>
+
+
+        <div style="height: 50px; margin-top: 0px;">
+        <div style="height: 21px; width: 160px; margin-left: 9px; margin-top: 19px;">
+            <asp:Label ID="Label3" runat="server" Text="Payments per Year"></asp:Label>
+        </div>
+        <div style="height: 0px; margin-top: -22px; width: 223px; margin-left: 200px;">          
+            <asp:TextBox ID="TextBox3" runat="server" Height="20px" Width="176px" TextMode="Number" OnTextChanged="TextBox3_TextChanged" ></asp:TextBox>
+        </div>
+        </div>
+
+
+        <div style="height: 50px">
+        <div style="height: 21px; width: 160px; margin-left: 9px; margin-top: 19px;">
+            <asp:Label ID="Label4" runat="server" Text="Down Payment"></asp:Label>
+        </div>
+        <div style="height: 0px; margin-top: -22px; width: 223px; margin-left: 200px;">          
+            <asp:TextBox ID="TextBox4" runat="server" Height="20px" Width="176px" TextMode="Number" OnTextChanged="TextBox4_TextChanged" ></asp:TextBox>
+        </div>
+        </div>
+
+
+         <div style="height: 50px">
+        <div style="height: 21px; width: 160px; margin-left: 9px; margin-top: 19px;">
+                <asp:Label ID="Label7" runat="server" Text="Interest Rate(%)"></asp:Label>
+        </div>
+        <div style="height: 0px; margin-top: -22px; width: 223px; margin-left: 200px;">          
+            <asp:TextBox ID="TextBox7" runat="server" Height="20px" Width="176px" TextMode="Number" OnTextChanged="TextBox7_TextChanged" ></asp:TextBox>
+        </div>
+        </div>
+
+        <div style="height: 50px">
+        <div style="height: 29px; width: 160px; margin-left: 9px; margin-top: 19px;">
+                <asp:Button ID="Button3" runat="server" Text="Show Min. Payment" Height="27px" Width="146px" OnClick="Button3_Click" />
+        </div>
+        <div style="height: 0px; margin-top: -28px; width: 223px; margin-left: 200px;">          
+            <asp:TextBox ID="TextBox5" runat="server" Height="20px" Width="176px"></asp:TextBox>
+        </div>
+        </div>
+
+
+        <div style="height: 50px">
+        <div style="height: 21px; width: 160px; margin-left: 9px; margin-top: 19px;">
+                <asp:Label ID="Label6" runat="server" Text="Additional Payments"></asp:Label>
+        </div>
+        <div style="height: 0px; margin-top: -22px; width: 223px; margin-left: 200px;">          
+            <asp:TextBox ID="TextBox6" runat="server" Height="20px" Width="176px" TextMode="Number" OnTextChanged="TextBox6_TextChanged" ></asp:TextBox>
+        </div>
+        </div>
+
+
+        <div style="height: 35px">
+            <div style="height: 30px; margin-top: 4px; width: 223px; margin-left: 200px;">          
+                <asp:RadioButton ID="RadioButton1" runat="server" OnCheckedChanged="RadioButton1_CheckedChanged" Text="Recurring Increment" EnableViewState="False"/>
+            </div>
+        </div>
+
+
+        <div style="margin-top: 0px">
+            <div style="width: 184px; margin-top: 16px; height: 33px;">
+                <asp:Button ID="Button1" runat="server" Text="Calculate" Height="27px" Width="118px" OnClick="Button1_Click" />
+            </div>
+            <div style="width: 184px; margin-top: -28px; height: 33px; margin-left: 220px;">
+                <asp:Button ID="Button2" runat="server" Text="Reset" Height="27px" Width="118px" OnClick="Button2_Click" />
+            </div>
+        </div>        
+
+        <asp:Table ID="Table1" runat="server" GridLines="Both" CellPadding="1" CellSpacing="1" Height="146px" Width="720px">
+        </asp:Table>
+
     </div>
 
-    <div class="row">
-        <div class="col-md-4">
-            <h2>Getting started</h2>
-            <p>
-                ASP.NET Web Forms lets you build dynamic websites using a familiar drag-and-drop, event-driven model.
-            A design surface and hundreds of controls and components let you rapidly build sophisticated, powerful UI-driven sites with data access.
-            </p>
-            <p>
-                <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301948">Learn more &raquo;</a>
-            </p>
-        </div>
-        <div class="col-md-4">
-            <h2>Get more libraries</h2>
-            <p>
-                NuGet is a free Visual Studio extension that makes it easy to add, remove, and update libraries and tools in Visual Studio projects.
-            </p>
-            <p>
-                <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301949">Learn more &raquo;</a>
-            </p>
-        </div>
-        <div class="col-md-4">
-            <h2>Web Hosting</h2>
-            <p>
-                You can easily find a web hosting company that offers the right mix of features and price for your applications.
-            </p>
-            <p>
-                <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301950">Learn more &raquo;</a>
-            </p>
-        </div>
-    </div>
-
+    
+   
 </asp:Content>
--- Default.aspx.cs
+++ Default.aspx.cs
@@ -9,9 +9,283 @@ namespace Mortgage_Calculator
 {
     public partial class _Default : Page
     {
+        double mortgage_amt;
+        double years;
+        double paymentPerYear;
+        double downpayment;
+        double requiredPayment;
+        double rate;
+        bool recurring = false;
+        double extraPay;
+
+
+
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
+            if (Double.TryParse(Session["Mortgage Amt"].ToString(), out mortgage_amt))
+            {
+                TextBox1.ForeColor = System.Drawing.Color.Black;
+            }
+
+            else
+            {
+                TextBox1.ForeColor = System.Drawing.Color.Red;
+                TextBox1.Text = "Invalid Input";
+            }
+
+            if (Double.TryParse(Session["Years"].ToString(), out years)) 
+            {
+                TextBox2.ForeColor = System.Drawing.Color.Black;
+            }
+
+            else
+            {
+                TextBox2.ForeColor = System.Drawing.Color.Red;
+                TextBox2.Text = "Invalid Input";
+            }
+
+            if ( Double.TryParse(Session["Payments Per year"].ToString(), out paymentPerYear))
+            {
+                TextBox3.ForeColor = System.Drawing.Color.Black;
+            }
+
+            else
+            {
+                TextBox3.ForeColor = System.Drawing.Color.Red;
+                TextBox3.Text = "Invalid Input";
+            }
+
+            if (Double.TryParse(Session["DownPayment"].ToString(), out downpayment))
+            {
+                TextBox4.ForeColor = System.Drawing.Color.Black;
+            }
+
+            else
+            {
+                TextBox4.ForeColor = System.Drawing.Color.Red;
+                TextBox4.Text = "Invalid Input";
+            }
+
+            if (Double.TryParse(Session["Rate"].ToString(), out rate)) 
+            {
+                TextBox7.ForeColor = System.Drawing.Color.Black;
+            }
+
+            else
+            {
+                TextBox7.ForeColor = System.Drawing.Color.Red;
+                TextBox7.Text = "Invalid Input";
+            }
+
+            if (Double.TryParse(Session["Extra Payment"].ToString(), out extraPay))
+            {
+                TextBox6.ForeColor = System.Drawing.Color.Black;
+            }
+
+            else
+            {
+                TextBox6.ForeColor = System.Drawing.Color.Red;
+                TextBox6.Text = "Invalid Input";
+            }
+
+
+
+
+        }
+
+        protected void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (RadioButton1.Checked)
+            {
+                recurring = true;
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //Create table with the required columns.
+            CreateHeaderRow();
+
+            int term = 0;
+            int cells = 6;
+            double interestAmt = 0;
+            double principalAmt = 0;
+            while (mortgage_amt > 0)
+            {
+                CreateValueRow(cells, term, requiredPayment, extraPay, interestAmt, principalAmt, mortgage_amt);
+                interestAmt = rate / (100 * paymentPerYear) * mortgage_amt;
+                principalAmt = requiredPayment - interestAmt;
+                mortgage_amt -= principalAmt;
+                term += 1;
+
+
+            }
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text = string.Empty;
+            TextBox2.Text = string.Empty;
+            TextBox3.Text = string.Empty;
+            TextBox4.Text = string.Empty;
+            TextBox5.Text = string.Empty;
+            TextBox6.Text = string.Empty;
+            TextBox7.Text = string.Empty;
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            requiredPayment = CalcMinPayment();
+        }
+
+        private void CreateHeaderRow()
+        {
+            //Create table with the required columns.
+            TableRow columnRow = new TableRow();
+            Table1.Rows.Add(columnRow);
+
+
+            TableCell termCount = new TableCell();
+            termCount.Text = "Num. of Terms";
+            columnRow.Cells.Add(termCount);
+
+            TableCell monthlyPay = new TableCell();
+            monthlyPay.Text = "Monthly Payment";
+            columnRow.Cells.Add(monthlyPay);
+
+
+            TableCell extraPayment = new TableCell();
+            extraPayment.Text = "Extra Payment";
+            columnRow.Cells.Add(extraPayment);
+
+
+            TableCell paidInterest = new TableCell();
+            paidInterest.Text = "Paid Interest";
+            columnRow.Cells.Add(paidInterest);
+
+
+            TableCell paidPrincipal = new TableCell();
+            paidPrincipal.Text = "Paid Principal";
+            columnRow.Cells.Add(paidPrincipal);
+
+
+            TableCell loanRemaining = new TableCell();
+            loanRemaining.Text = "Loan Balance Remaining";
+            columnRow.Cells.Add(loanRemaining);
+        }
+
+        private void CreateValueRow(int cells, int termcount, double monthlypay, double extrapay, double interestpay, double principalpay, double remaining)
+        {
+            TableRow columnRow = new TableRow();
+            Table1.Rows.Add(columnRow);
+            if (termcount == 0)
+            {
+                for (int i = 1; i < cells;  i++)
+                {
+
+                    TableCell termCount = new TableCell();
+                    columnRow.Cells.Add(termCount);
+
+                    TableCell monthlyPay = new TableCell();
+                    columnRow.Cells.Add(monthlyPay);
+
+
+                    TableCell extraPayment = new TableCell();
+                    columnRow.Cells.Add(extraPayment);
+
+
+                    TableCell paidInterest = new TableCell();
+                    columnRow.Cells.Add(paidInterest);
+
+
+                    TableCell paidPrincipal = new TableCell();
+                    columnRow.Cells.Add(paidPrincipal);
+                }
+                TableCell loanRemaining = new TableCell();
+                loanRemaining.Text = remaining.ToString();
+                columnRow.Cells.Add(loanRemaining);
+            }
+            else
+            {
+                TableCell termCount = new TableCell();
+                termCount.Text = termcount.ToString();
+                columnRow.Cells.Add(termCount);
+
+                TableCell monthlyPay = new TableCell();
+                monthlyPay.Text = monthlypay.ToString();
+                columnRow.Cells.Add(monthlyPay);
+
+
+                TableCell extraPayment = new TableCell();
+                extraPayment.Text = extrapay.ToString();
+                columnRow.Cells.Add(extraPayment);
+
+
+                TableCell paidInterest = new TableCell();
+                paidInterest.Text = interestpay.ToString();
+                columnRow.Cells.Add(paidInterest);
+
+
+                TableCell paidPrincipal = new TableCell();
+                paidPrincipal.Text = principalpay.ToString();
+                columnRow.Cells.Add(paidPrincipal);
+
+
+                TableCell loanRemaining = new TableCell();
+                loanRemaining.Text = remaining.ToString();
+                columnRow.Cells.Add(loanRemaining);
+
+            }
+        }
+
+        private double CalcMinPayment()
+        {
+            if(mortgage_amt > 0 && years > 0 && paymentPerYear > 0 && downpayment >= 0 && rate > 0)
+            {
+                TextBox5.ForeColor = System.Drawing.Color.Black;
+                TextBox5.Text = Math.Ceiling(mortgage_amt * (rate / (100 * paymentPerYear))* Math.Pow(1 + (rate / (100 * paymentPerYear)),years * paymentPerYear)/
+                                (Math.Pow(1 + (rate / (100 * paymentPerYear)), years * paymentPerYear) -1 )).ToString();
+                return double.Parse(TextBox5.Text);
+            }
+            else
+            {
+                TextBox5.ForeColor = System.Drawing.Color.Red;
+                TextBox5.Text = "Invalid Input/s";               
+                return 0;
+            }
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            Session["Mortgage Amt"] = TextBox1.Text;
+        }
+
+        protected void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            Session["Years"] = TextBox2.Text;
+        }
+
+        protected void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+            Session["Payments Per year"] = TextBox3.Text;
+        }
+
+        protected void TextBox4_TextChanged(object sender, EventArgs e)
+        {
+            Session["DownPayment"] = TextBox4.Text;
+        }
+
+        protected void TextBox7_TextChanged(object sender, EventArgs e)
+        {
+            Session["Rate"] = TextBox7.Text;
+        }
+
+        protected void TextBox6_TextChanged(object sender, EventArgs e)
+        {
+            Session["Extra Payment"] = TextBox6.Text;
         }
     }
 }
